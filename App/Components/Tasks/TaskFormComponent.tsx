133f7e0c5dee8a92cde9055b1c976002bf06f773
Nicolas Pirotte <=>
2016-02-15 07:35:03 +0100

    clean form form example

--- App/Components/Tasks/TaskFormComponent.tsx
+++ App/Components/Tasks/TaskFormComponent.tsx
@@ -1,7 +1,7 @@
 /// <reference path="../../../typings/tsd.d.ts" />
 
 import React = require("react");
-import TaskActionCreator from "../../ActionCreators/TaskActionCreator";
+// import TaskActionCreator from "../../ActionCreators/TaskActionCreator";
 
 interface IState {
   name: string;
@@ -10,9 +10,6 @@ interface IState {
 export default class TaskFormComponent extends React.Component<{}, IState> {
   constructor() {
     super();
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-
     this.state = {
       name: null
     };
@@ -20,28 +17,9 @@ export default class TaskFormComponent extends React.Component<{}, IState> {
 
   render(): React.ReactElement<{}> {
     return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            ref="task"
-            value={this.state.name}
-            onChange={this.handleChange}
-            className="form-control"
-            placeholder="Add a new task" />
-        </form>
+      <div className="TaskFormComponent">
+
       </div>
     );
-  }
-
-  handleChange(e: React.KeyboardEvent): void {
-    const target: HTMLInputElement = e.target as HTMLInputElement;
-    this.setState({name: target.value});
-  }
-
-  handleSubmit(e: React.FormEvent): void {
-    e.preventDefault();
-    TaskActionCreator.add(this.state.name);
-    this.setState({name: null});
   }
 }
