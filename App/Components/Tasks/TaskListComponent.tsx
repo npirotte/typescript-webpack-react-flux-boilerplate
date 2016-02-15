820b6b2eb0b032653187b4f119bbf117424347e2
Nicolas Pirotte <=>
2016-02-15 07:40:52 +0100

    remove TaskComponent for example

--- App/Components/Tasks/TaskListComponent.tsx
+++ App/Components/Tasks/TaskListComponent.tsx
@@ -2,9 +2,9 @@
 
 import React = require("react");
 
-import {taskStore, TTaskList, TTask} from "../../Stores/TaskStore";
+import {taskStore, TTaskList} from "../../Stores/TaskStore";
 
-import TaskComponent from "./TaskComponent";
+// import TaskComponent from "./TaskComponent";
 
 interface IState {
   tasks: TTaskList;
@@ -30,10 +30,8 @@ export default class TaskListComponent extends React.Component<{}, IState> {
 
   render (): React.ReactElement<{}> {
     return (
-      <div className="list-group">
-        {this.state.tasks.map((task: TTask): React.ReactElement<{}> => {
-          return <TaskComponent task={task} key={task.get("id")} />;
-        }).toArray()}
+      <div className="TaskListComponent list-group">
+
       </div>
     );
   }
